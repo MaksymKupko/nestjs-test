a23de47a5be3d664371df16686d7f33c8c77eecd
Maksym Kupko <maksym.kupko@codeandcakes.com>
2024-01-07 23:38:46 +0200

    feat: add UserModule

---
+++ src/modules/user/entities/user.entity.ts
@@ -0,0 +1,19 @@
+import { Exclude } from 'class-transformer';
+import { Column, Entity } from 'typeorm';
+import { StarterEntity } from '../../../shared/base.entity';
+
+@Entity('users')
+export class User extends StarterEntity {
+  @Column()
+  email: string;
+
+  @Column()
+  first_name: string;
+
+  @Column()
+  last_name: string;
+
+  @Column()
+  @Exclude()
+  password: string;
+}
